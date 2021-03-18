746ce689c746ff2fa2dbeb50c1ec115a6cd54315
1KVueltasAlCampo <angrybirdsgallo@gmail.com>
2021-03-18 08:42:55 -0500

    12: Added user control methods to the Restaurant class

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -1,1 +1,73 @@
 package model;
+
+import java.util.ArrayList;
+
+public class Restaurant{
+	private User rootUser;
+	private boolean firstTime;
+	private ArrayList<User> users;
+	private int usersSize;
+	private User currentUser;
+	public Restaurant() {
+		rootUser = new User("Generic","user","none","Root","admin");
+		firstTime = true;
+		currentUser = rootUser;
+		users = new ArrayList<User>();
+		usersSize=users.size();
+	}
+	
+	/**
+	 * Checks if the user that is being used isn't the root user.<br>
+	 * <b>Pre: </b><br>
+	 * <b>Post: </b>It is stated whether or not the root user is being used<br>
+	 * @return true if the current user is the root user, false if not
+	 */
+	public boolean checkFirstTime() {
+		if(firstTime&&currentUser.equals(rootUser)) {
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * Changes the user that is being used.<br>
+	 * <b>Pre: </b>Needs an index indicating what user will be used<br>
+	 * <b>Post: </b>The user is changed<br>
+	 * @param index The index of the user in the users ArrayList
+	 * @return true if the user was changed, false if not
+	 */
+	public boolean changeUser(int index) {
+		if(index!=-1) {
+			this.currentUser=users.get(index);
+			return true;
+		}
+		else if(index==-2) {
+			this.currentUser=rootUser;
+			return true;
+		}
+		return false;
+		
+	}
+	/**
+	 * Searchs for a user from the ArrayList or uses the rootUser<br>
+	 * <b>Pre: </b>There must be at least one user <br>
+	 * <b>Post: </b>Finds the user if it existed<br>
+	 * @param username The username of the user that will be searched
+	 * @param password The password of the user that will be searched
+	 * @return index
+	 */
+	public int login(String username,String password) {
+		int index = -1;
+		boolean band = false;
+		for(int i=0;i<usersSize&&!band;i++) {
+			if((users.get(i).getUsername().equals(username))&&(users.get(i).getPassword().equals(password))) {
+				index = i;
+				band = true;
+			}
+		}
+		if(rootUser.getUsername().equals(username)&&rootUser.getPassword().equals(password)) {
+			index = -2;
+		}
+		return index;
+	}
+	
+}
