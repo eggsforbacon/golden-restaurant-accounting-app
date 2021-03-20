6a936d91746504015a0449b863c27e14557b271f
Samuel Hernandez <doicirohangnan@gmail.com>
2021-03-20 15:47:58 -0500

    21: UI patch

--- src/ui/CenterPanesGUIController.java
+++ src/ui/CenterPanesGUIController.java
@@ -1,17 +1,42 @@
 package ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import model.*;
 
 public class CenterPanesGUIController {
-
+    /*Product pane*/
     @FXML
     private TableView<Product> productTBV;
 
     @FXML
     private TableColumn<Product, String> nameCol;
+
+    @FXML
+    private TableColumn<Product, String> enabledCol;
+
+    @FXML
+    private TableColumn<Product, String> ingredientsCol;
+
+    @FXML
+    private TableColumn<Product, String> sPriceCol;
+
+    @FXML
+    private TableColumn<Product, String> mPriceCol;
+
+    @FXML
+    private TableColumn<Product, String> lPriceCol;
+
+    @FXML
+    private Label spacer2;
+
+    @FXML
+    private Label spacer3;
+
+    @FXML
+    private Label lastUserLBL;
 
 
 }
