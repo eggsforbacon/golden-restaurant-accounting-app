3f9046675843acd602ce0a2b2cc6dfac8c482d07
Samuel Hernandez <doicirohangnan@gmail.com>
2021-03-21 00:13:28 -0500

    26: - Add product window styled

--- src/ui/CenterPanesGUIController.java
+++ src/ui/CenterPanesGUIController.java
@@ -4,12 +4,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import model.PlateType;
 import model.Product;
 import model.Restaurant;
@@ -17,6 +25,7 @@ import model.User;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class CenterPanesGUIController implements Initializable {
@@ -34,7 +43,7 @@ public class CenterPanesGUIController implements Initializable {
     private TableColumn<Product, String> ingredientsCol;
 
     @FXML
-    private TableColumn<Product, PlateType> typeCol;
+    private TableColumn<Product, String> typeCol;
 
     @FXML
     private TableColumn<Product, ArrayList<String>> sizesCol;
@@ -99,6 +108,43 @@ public class CenterPanesGUIController implements Initializable {
     void editType(ActionEvent event) {
 
     }
+
+    @FXML
+    void addProductCLicked(ActionEvent event) {
+        try {
+            System.out.println("We tried");
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("addProduct.fxml"));
+            fxmlLoader.setController(this);
+            Parent root = fxmlLoader.load();
+            Stage createProduct = new Stage();
+            createProduct.setScene(new Scene(root));
+            createProduct.initModality(Modality.APPLICATION_MODAL);
+            //createProduct.initStyle(StageStyle.UTILITY);
+            Image icon = new Image(String.valueOf(getClass().getResource("resources/gh-icon.png")));
+            createProduct.getScene().getStylesheets().addAll(String.valueOf(getClass().getResource("css/stylesheet.css")));
+            createProduct.getIcons().add(icon);
+            createProduct.setTitle("Golden Restaurant: Añadir producto");
+            createProduct.setResizable(false);
+            createProduct.show();
+        } catch (Exception e) {
+            System.out.println("Can't load window at the moment.");
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    private BorderPane productPane;
+
+    @FXML
+    void cancelProduct(ActionEvent event) {
+
+    }
+
+    @FXML
+    void confirmProduct(ActionEvent event) {
+
+    }
+
 
     @FXML
     void fullProductDetails(MouseEvent event) {
