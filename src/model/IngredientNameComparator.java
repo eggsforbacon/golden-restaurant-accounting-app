8b8b4e94dba2ef04786fb8294588ec8801203a0a
1KVueltasAlCampo <angrybirdsgallo@gmail.com>
2021-04-08 08:07:10 -0500

    95. Fixed error with sort of ingredients

--- src/model/IngredientNameComparator.java
+++ src/model/IngredientNameComparator.java
@@ -4,7 +4,7 @@ public class IngredientNameComparator implements Comparator<Ingredient> {
 
 	@Override
 	public int compare(Ingredient o1, Ingredient o2) {
-		return o1.getName().compareTo(o2.getName());
+		return -1*(o1.getName().compareTo(o2.getName()));
 	}
 
 }
